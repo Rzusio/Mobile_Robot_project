5ba7344d8dc7e3580165665cca48d6c98ca95645
Mateusz Rzuchowski <rzucho5@o2.pl>
2023-01-19 15:05:50 +0100

    second commit

---
+++ WindowsFormsApp1-20220607T105304Z-001/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,407 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_diody = new System.Windows.Forms.TextBox();
+            this.button_odbierz = new System.Windows.Forms.Button();
+            this.textBox_odbierz = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wyslij ramke";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(430, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ramka";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(783, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Zielone";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(683, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 58);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Czerwone";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(732, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Zapalanie LED";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(751, 313);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "↑";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(795, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 45);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "→";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(712, 358);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 45);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "←";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(738, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sterowanie";
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightGray;
+            this.button7.Location = new System.Drawing.Point(783, 162);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 44);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Obie";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(31, 291);
+            this.trackBar1.Maximum = 127;
+            this.trackBar1.Minimum = -128;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(160, 69);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Wartość ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(216, 317);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(216, 367);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 15;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(39, 162);
+            this.trackBar2.Maximum = 127;
+            this.trackBar2.Minimum = -128;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(152, 69);
+            this.trackBar2.TabIndex = 16;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(198, 148);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(197, 192);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Wartość";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Silnik lewy";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Silnik prawy";
+            // 
+            // textBox_diody
+            // 
+            this.textBox_diody.Location = new System.Drawing.Point(727, 220);
+            this.textBox_diody.Name = "textBox_diody";
+            this.textBox_diody.Size = new System.Drawing.Size(100, 26);
+            this.textBox_diody.TabIndex = 22;
+            this.textBox_diody.TextChanged += new System.EventHandler(this.textBox_diody_TextChanged);
+            // 
+            // button_odbierz
+            // 
+            this.button_odbierz.Location = new System.Drawing.Point(50, 428);
+            this.button_odbierz.Name = "button_odbierz";
+            this.button_odbierz.Size = new System.Drawing.Size(128, 67);
+            this.button_odbierz.TabIndex = 23;
+            this.button_odbierz.Text = "Odbierz ramke";
+            this.button_odbierz.UseVisualStyleBackColor = true;
+            this.button_odbierz.Click += new System.EventHandler(this.button_odbierz_Click);
+            // 
+            // textBox_odbierz
+            // 
+            this.textBox_odbierz.Location = new System.Drawing.Point(50, 523);
+            this.textBox_odbierz.Name = "textBox_odbierz";
+            this.textBox_odbierz.Size = new System.Drawing.Size(430, 26);
+            this.textBox_odbierz.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(421, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 20);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "RamkaManual";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(433, 249);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 26;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(445, 300);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 52);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "Wyślij ramke";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(709, 440);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(138, 97);
+            this.button9.TabIndex = 29;
+            this.button9.Text = "STOP";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(667, 162);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(94, 44);
+            this.button10.TabIndex = 30;
+            this.button10.Text = "zadne";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 602);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_odbierz);
+            this.Controls.Add(this.button_odbierz);
+            this.Controls.Add(this.textBox_diody);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_diody;
+        private System.Windows.Forms.Button button_odbierz;
+        private System.Windows.Forms.TextBox textBox_odbierz;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ WindowsFormsApp1-20220607T105304Z-001/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                string hostname = "127.0.0.1";
+                int port = 8000;
+                s.Connect(hostname, port);
+                MessageBox.Show("Polaczono");
+            }
+            catch
+            {
+                MessageBox.Show("Nie polaczono");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string ramka = textBox_diody.Text + textBox3.Text + textBox5.Text + "]";
+            textBox1.Text = ramka;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ramka = textBox_diody.Text + textBox3.Text + textBox5.Text + "]";
+            textBox1.Text = ramka;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(textBox_diody.Text + textBox3.Text + textBox5.Text +"]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[01";
+            textBox_diody.Text = rozkaz;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "0000]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[02";
+            textBox_diody.Text = rozkaz;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "0000]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string rozkaz = textBox_diody.Text;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "1010]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[00";
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "1000]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[00";
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "0010]");
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[03";
+            textBox_diody.Text = rozkaz;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "0000]");
+
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text =  trackBar1.Value.ToString();
+            int value = Convert.ToInt32(trackBar1.Value);
+            if (value < 0)
+            {
+                value = value + 255;
+            }
+            string ramka2 = String.Format("{0:X}", value);
+            
+            textBox3.Text = ramka2.ToString();
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            textBox4.Text = trackBar2.Value.ToString();
+            int value = Convert.ToInt32(trackBar2.Value);
+            if (value < 0)
+            {
+                value = value + 255;
+            }
+            string ramka3 = String.Format("{0:X}", value);
+
+            textBox5.Text = ramka3.ToString();
+            
+        }
+
+        private void textBox_diody_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_odbierz_Click(object sender, EventArgs e)
+        {
+            byte[] przyjmij = new byte[256];
+            NetworkStream odbierz = new NetworkStream(s);
+            odbierz.Read(przyjmij, 0, 256);
+            string returndata = System.Text.Encoding.ASCII.GetString(przyjmij);
+            textBox_odbierz.Text = returndata;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string ramka = textBox6.Text;
+            textBox6.Text = ramka;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(ramka);
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+        }
+
+       
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[00";
+            textBox_diody.Text = rozkaz;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "0000]");
+
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string rozkaz = "[00";
+            textBox_diody.Text = rozkaz;
+            Byte[] ramka1 = Encoding.ASCII.GetBytes(rozkaz + "]");
+
+            NetworkStream wyslij = new NetworkStream(s);
+            wyslij.Write(ramka1, 0, ramka1.Length);
+        }
+    }
+}
